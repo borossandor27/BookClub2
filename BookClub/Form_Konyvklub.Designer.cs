1f503e8a9fd6b23b329b67f8f76ef3384fb49d73
borossandor27 <boros.sanyi@gmail.com>
2024-10-05 13:48:20 +0200

    Add project files.

---
+++ BookClub/Form_Konyvklub.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BookClub
+{
+    partial class Form_Konyvklub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tiltás / Tiltás visszavonása";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGV
+            // 
+            this.dataGV.AllowUserToAddRows = false;
+            this.dataGV.AllowUserToDeleteRows = false;
+            this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGV.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGV.Location = new System.Drawing.Point(0, 76);
+            this.dataGV.Name = "dataGV";
+            this.dataGV.ReadOnly = true;
+            this.dataGV.RowHeadersWidth = 51;
+            this.dataGV.RowTemplate.Height = 24;
+            this.dataGV.Size = new System.Drawing.Size(800, 374);
+            this.dataGV.TabIndex = 1;
+            // 
+            // Form_Konyvklub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGV);
+            this.Controls.Add(this.button1);
+            this.Name = "Form_Konyvklub";
+            this.Text = "Konyvklub";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGV;
+    }
+}
+
---
+++ BookClub/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub
+{
+    internal class Member
+    {
+        int id;
+        string name;
+        enum gender { male, female, other };
+        DateTime birth_date;
+        bool banned;
+
+        public Member(int id, string name, string nem, DateTime birth_date, bool banned)
+        {
+            this.id = id;
+            this.name = name;
+            this.birth_date = birth_date;
+            this.banned = banned;
+            this.GetGender(nem);
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public DateTime Birth_date { get => birth_date; set => birth_date = value; }
+        public bool Banned { get => banned; set => banned = value; }
+        gender GetGender(string input)
+        {
+            switch (input)
+            {
+                case "M":
+                    return gender.male;
+                case "F":
+                    return gender.female;
+               default:
+                    return gender.other; // Ha más karaktereket kapsz vagy nem megfelelő karaktereket, akkor a nemet "other"-re állítja
+            }
+        }
+    }
+}
